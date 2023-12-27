6b51b6a45e7a4a9ce1b3e9171c8897fe3d067964
aavaSapkota <sapkota.aava04@gmail.com>
2023-12-27 17:58:51 -0500

    delete old style

--- mailto.js
+++ mailto.js
@@ -5,5 +5,5 @@ function mailto(){
     for(let item of document.getElementsByClassName("input-field")){
         if(item.value != "SUBMIT") message += item.value + "%0A";
     }
-    emailWindow = window.open("mailto:aavachhori@gmail.com?subject=Aava%20Sapkota's%20Website%20Form%20Response&body="+message);
+    emailWindow = window.open("mailto:sapkota.aava04@gmail.com?subject=Aava%20Sapkota's%20Website%20Form%20Response&body="+message);
 }
